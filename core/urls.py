c708bc04580765caeb1ad75fa356ca3532a48ec3
frankto5 <francojb5@hotmail.com>
2021-09-06 02:56:24 -0300

    movie-app ended

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
+from movieapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView, name='HomeView'),
+    path('movies/<slug:slug>',MovieDetailView.as_view(),name='MovieDetailView'),
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from movieapp.models import *
 
 def HomeView(request):
-    return render(request,'index.html')
+    rec_movies = Movie.objects.filter(recommended=True)
+    prem_movies = Movie.objects.filter(premiere=True) 
+    context = {
+        'rec_movies':rec_movies,
+        'prem_movies':prem_movies
+    }
+    return render(request,'index.html',context)
